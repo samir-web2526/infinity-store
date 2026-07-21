44ae5c3c53fbee7902fa08e0ed162363a23fe328
samir-web2526 <baishnabsamir26@gmail.com>
2026-07-21 16:56:50 +0600

    error fixed on deploy time

--- infinity-store-client/src/hooks/useTheme.js
+++ infinity-store-client/src/hooks/useTheme.js
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { ThemeContext } from "./themeContextValue";
+import { ThemeContext } from "@/context/themeContextValue";
 
 export const useTheme = () => useContext(ThemeContext);
